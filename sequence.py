67368fccd0d63c78b03454bcddb50e42c4e0452b
madsaken <haffisaken@gmail.com>
2019-09-05 10:18:10 +0000

    1.0.0 sequence

---
+++ sequence.py
@@ -0,0 +1,18 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+first_int = 1
+sec_int = 2
+third_int = 3
+storage = 0
+
+print(first_int)
+print(sec_int)
+print(third_int)
+
+for i in range(1,n):
+
+    storage = first_int + sec_int + third_int
+    first_int = sec_int
+    sec_int = third_int
+    third_int = storage
+    print(storage)
